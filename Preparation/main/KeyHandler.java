b29c55a4c9c9b09339cf1d3c30fd90b67a4b0aa1
hvb1412 <hoangvanbinh141205@gmail.com>
2025-04-07 22:21:30 +0700

    Add files via upload

---
+++ Preparation/main/KeyHandler.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener
+{
+    GamePanel gp;
+
+    public boolean upPressed, downPressed, leftPressed, rightPressed, attackJPressed;
+
+    public KeyHandler(GamePanel gp)
+    {
+        this.gp = gp;
+    }
+
+    public void keyTyped(KeyEvent e)
+    {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
+            rightPressed = true;
+        }
+        if (code == KeyEvent.VK_J) {
+            attackJPressed = true;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W || code == KeyEvent.VK_UP)
+        {
+            upPressed = false;
+        }
+        if(code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT)
+        {
+            leftPressed = false;
+        }
+        if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN)
+        {
+            downPressed = false;
+        }
+        if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT)
+        {
+            rightPressed = false;
+        }
+
+    }
+}
