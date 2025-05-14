0c8989483f2df02366e58ce913d667f170cba599
PhamNam342 <nam1234kan@gmail.com>
2025-05-14 19:28:57 +0700

    fix sound

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -10,21 +10,26 @@ public class Sound {
 	Clip clip;
 	URL soundURL[] = new URL[30];
 	
-	public Sound() {
-		soundURL[0] =getClass().getResource("/sound/And The Journey Begins.wav");
-		soundURL[1] =getClass().getResource("/sound/CollectItem.wav");
-		soundURL[2] =getClass().getResource("/sound/gameover.wav");
-		soundURL[3] =getClass().getResource("/sound/MonsterSword.wav");
-		soundURL[4] =getClass().getResource("/sound/player_attack.wav");
-		soundURL[5] =getClass().getResource("/sound/recievedamage.wav");
-		soundURL[6] =getClass().getResource("/sound/teleport.wav");
+	public Sound(){
+		soundURL[0] =getClass().getResource("res/sounds/teleport.wav");
+		//soundURL[1] =getClass().getResource("res/sounds/CollectItem.wav");
+		//soundURL[2] =getClass().getResource("res/sounds/gameover.wav");
+		//soundURL[3] =getClass().getResource("/sounds/MonsterSword.mp3");
+		//soundURL[4] =getClass().getResource("res/sounds/player_attack.wav");
+		//soundURL[5] =getClass().getResource("res/sounds/receivedamage.wav");
+		//soundURL[6] =getClass().getResource("res/sounds/teleport.wav");
 	}
 	public void setFile(int i) {
 		try {
+			if(soundURL[i] == null) {
+				System.out.println("wrong");
+			}
 			AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
 			clip = AudioSystem.getClip();
 			clip.open(ais);
-		}catch(Exception e);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	public void play() {
 		clip.start();
